ac87651a6daae3ce8e8251e9d88cecbfbfebacd6
alexsouzajunior <alexsandrosouzajr10@gmail.com>
2024-10-02 18:47:53 -0300

    modificações no portfólio

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,12 @@
+const body = document.querySelector('body');
 const burger = document.querySelector('.burger');
 const menu = document.getElementById('menu');
 const closeMenu = document.querySelector('.close-menu');
 const overlay = document.getElementById('overlay');
 const navLinks = document.querySelectorAll('.navlinks a');
+const darkModeIcons = document.querySelector('.darkmode__icons');
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
 const sections = document.querySelectorAll('section');
 const filterBtn = document.querySelectorAll('.btn-filter');
 const filterProjects = document.querySelectorAll('.projects__cards .project__card');
@@ -10,9 +14,37 @@ const topo = document.querySelector('.topo');
 const openModal = document.getElementById('modal-email');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.closeModal');
-const overlayModal = document.getElementById('overlay-modal')
-const contactCardEmail = document.querySelector('.email')
-const contactCardEmailIcon = document.querySelector('.email svg')
+const overlayModal = document.getElementById('overlay-modal');
+const contactCardEmail = document.querySelector('.email');
+const contactCardEmailIcon = document.querySelector('.email svg');
+let darkmode = localStorage.getItem("dark-mode");
+
+ function darkModeEnabled() {
+    sun.classList.add('active');
+    moon.classList.add('active');
+    localStorage.setItem("dark-mode", 'Modo escuro ativado');
+    body.classList.add('darkmode');
+}
+
+ function darkModeDisabled() {
+    sun.classList.remove('active');
+    moon.classList.remove('active');
+    localStorage.setItem("dark-mode", 'Modo escuro desativado');
+    body.classList.remove('darkmode');
+}
+
+if (darkmode === 'Modo escuro ativado') {
+    darkModeEnabled();
+}
+
+darkModeIcons.addEventListener('click', () => {
+    darkmode = localStorage.getItem("dark-mode");
+    if (darkmode === 'Modo escuro desativado') {
+        darkModeEnabled();
+    } else {
+        darkModeDisabled();
+    }
+});
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active");
@@ -48,16 +80,10 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// var typed = new Typed(".type", {
-//     strings: ["Alexsandro"],
-//     typeSpeed: 70,
-//     backSpeed: 70,
-// });
-
 this.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -97,3 +123,54 @@ closeModal.addEventListener('click', () => {
     contactCardEmail.classList.remove('active')
     contactCardEmailIcon.classList.remove('active')
 });
+
+ScrollReveal({
+    distance: '60px',
+    duration: 1500,
+    delay: 400
+});
+
+ScrollReveal().reveal('.main-content .socials a', { delay: 2000, origin: 'bottom' });
+ScrollReveal().reveal('.main-content h2', { delay: 1500, origin: 'bottom' });
+ScrollReveal().reveal('.main-content h4', { delay: 1000, origin: 'bottom' });
+ScrollReveal().reveal('.main-content .buttons', { delay: 500, origin: 'bottom' });
+ScrollReveal().reveal('.main .imageBox', { delay: 250, origin: 'right' });
+
+ScrollReveal().reveal('.about-content h2', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.about-content p', { delay: 500, origin: 'top' });
+
+ScrollReveal().reveal('.skills__title', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.skill', { interval: 300, scale: 0.85 });
+
+ScrollReveal().reveal('.box__text .heading', { delay: 400, origin: 'bottom' });
+ScrollReveal().reveal('.swiper-pagination', { delay: 800, scale: .75 });
+
+ScrollReveal().reveal('.contact__heading', { delay: 300, origin: 'bottom' });
+ScrollReveal().reveal('.contact__card', { interval: 250, origin: 'top' });
+
+var carousel = new Swiper('.carousel-projects', {
+    slidesPerView: 3,
+    navigation: {
+        prevEl: '.carousel-button-prev',
+        nextEl: '.carousel-button-next',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        240: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        680: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+})
